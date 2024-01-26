c7d1c3f83ba1e225c28920d0c1f805c125b52b79
brendantangluu <btangluu@my.bcit.ca>
2024-01-25 18:02:02 -0800

    Created GlobalContext (for favorites function)

---
+++ src/utilities/GlobalContext.js
@@ -0,0 +1,44 @@
+import {createContext, useEffect, useState} from "react";
+
+
+export const GlobalContext = createContext();
+
+export function GlobalProvider({children}){
+    function loadFromlocalStorage(){
+        const localData = localStorage.getItem("favorites");
+        return localData ? JSON.parse(localData) : [];
+    }
+
+    const [favorites, setFavorites] = useState(loadFromlocalStorage());
+
+    function addToFavorites(favorite){
+        const newFavorites = [...favorites, favorite];
+        setFavorites(newFavorites);
+    }
+
+
+    function removeFromFavorites(favorite){
+        const newFavorites = favorites.filter((fav)=>{
+            return fav.id !== favorite.id;
+        });
+        setFavorites(newFavorites);
+    }
+
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    },[favorites]);
+
+    return(
+        <GlobalContext.Provider
+            value={{
+                favorites: favorites,
+                addToFavorites: addToFavorites,
+                removeFromFavorites: removeFromFavorites,
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
+
+
+}
