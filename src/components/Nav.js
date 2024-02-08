1b9b4e55748f4e930979b8bc1a33be95180326da
brendantangluu <btangluu@my.bcit.ca>
2024-02-08 15:59:27 -0800

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,15 @@ import { NavLink } from 'react-router-dom';
 
 function Nav({ className }) {
   return (
-    <nav className={`bg-headerBG text-text w-full h-36 md:bg-none md:h-16 md:w-auto md:justify-end md:absolute md:top-4 md:right-16 ${className}`}>
-      <ul className='text-2xl text-center md:flex md:text-xl lg:text-2xl'>
-        <li className='m-4 hover:text-logo xl:mx-16'>
+    <nav className={`bg-headerBG text-text w-full h-44 ${className}`}>
+      <ul className='text-2xl text-center lg:text-3xl'>
+        <li className='m-4'>
           <NavLink to="/favourites">Favourites</NavLink>
         </li>
-        <li className='m-4 hover:text-logo xl:mx-16'>
+        <li className='m-4'>
           <NavLink to="/watchlist">Watch List</NavLink>
         </li>
-        <li className='m-4 hover:text-logo xl:mx-16'>
+        <li className='m-4'>
           <NavLink to="/about">About</NavLink>
         </li>
       </ul>
