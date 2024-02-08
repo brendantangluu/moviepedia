29643b32d6df6e57b12b1b4491b3c50e110c4c86
brendantangluu <btangluu@my.bcit.ca>
2024-02-07 19:18:03 -0800

    hamburger disappears when desktop size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef} from 'react';
 import Nav from './Nav';
-import Search from './Search';
 import Logo from './Logo';
+import SearchBar from './SearchBar';
 
 
 function Header() {
@@ -39,25 +39,22 @@ function Header() {
   };
 
   return (
-    <header className='bg-headerBG'>
-      <div className="flex justify-between items-center text-text">
+    <header className='bg-headerBG md:h-24 md:relative'>
+      <div className="flex justify-between items-center text-text md:h-24">
         <Logo/>
         <div className='flex'>
           <button onClick={() => setShowSearch(!showSearch)}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 pt-2 lg:hidden">
-              <path fill="#efefef" d="m18.9 20.3l-5.6-5.6q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.612 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l5.625 5.625q.275.275.275.675t-.3.7q-.275.275-.7.275t-.7-.275ZM9.5 14q1.875 0 3.188-1.313T14 9.5q0-1.875-1.313-3.188T9.5 5Q7.625 5 6.312 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14Z"></path>
-            </svg>
+          {/* <SearchBar
+          ref={searchRef}
+          onClick={handleInputClick}
+          className={`${showSearch ? 'block' : 'hidden'}`} /> */}
           </button>
-          <button onClick={() => setShowNav(!showNav)} className="text-4xl m-1 p-2">
+          <button onClick={() => setShowNav(!showNav)} className="text-4xl m-1 p-2 md:hidden">
             ☰
           </button>
         </div>
       </div>
-      <Search
-        ref={searchRef}
-        onClick={handleInputClick}
-        className={`${showSearch ? 'block' : 'hidden'}`} />
-      <Nav className={`${showNav ? 'block' : 'hidden'}`} />
+      <Nav className={`${showNav ? 'block' : 'hidden'} md:flex`} />
     </header>
   );
 }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 function Logo({className}){
     return (
-        <Link to="/" className="flex p-2 text-3xl">
+        <Link to="/" className="flex p-2 text-3xl md:text-5xl md:mx-4">
             <h2 className={className}>M</h2>
             <h2 className='text-logo'>P</h2>
         </Link>
