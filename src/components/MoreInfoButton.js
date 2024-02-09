392a32c3b8faf7f507ade07e02daa7331d5d4e2f
brendantangluu <btangluu@my.bcit.ca>
2024-02-09 13:24:48 -0800

    added hover for more info button

--- src/components/MoreInfoButton.js
+++ src/components/MoreInfoButton.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 
 function MoreInfoButton({movieData}){
+    const buttonStyles = "more-info-btn bg-logo text-white hover:bg-white hover:text-logo duration-300 px-1 py-1 rounded text-xs mt-2 uppercase font-bold sm:px-4 sm:py-2 sm:text-base md:mt-4 xl:mt-8 xl:px-6 xl:py-4 xl:text-2xl";
     return(
         <Link to={`/single/${movieData.id}/about`}>
-            <button className="more-info-btn bg-logo text-white px-1 py-1 rounded text-xs mt-2 hover:animate-pulse uppercase font-bold sm:px-4 sm:py-2 sm:text-base md:mt-4 xl:mt-8 xl:px-6 xl:py-4 xl:text-2xl">
-                More Info
+            <button className={buttonStyles}>
+               More Info
             </button>
         </Link>
     )
