ff9aa8d65dc5799d6975f2ff77aaa802165148c2
brendantangluu <btangluu@my.bcit.ca>
2024-02-09 09:34:22 -0800

    Added fallbacks to Favourites and Watchlist

--- src/pages/PageFavourites.js
+++ src/pages/PageFavourites.js
@@ -6,13 +6,22 @@ function Favourites() {
   const { favorites } = useContext(GlobalContext);
 
   return (
-  <div className="text-center">
+  <div className="text-center h-dvh">
       <h2 className="text-4xl font-bold my-4">Favourites</h2>
-      <div className="flex flex-wrap justify-center gap-12">
-        {favorites.map((favorite, index) => (
-          <MovieCard key={index} movieData={favorite} />
-        ))}
-      </div>
+      
+      {favorites.length > 0 && (
+        <div className="flex flex-wrap justify-center gap-12">
+          {favorites.map((favorite, index) => (
+            <MovieCard key={index} movieData={favorite} />
+          ))}
+        </div>
+      )}
+
+      {favorites.length === 0 && (
+        <p className='text-2xl'>No movies were added!</p>
+      )}
+
+
     </div>
   );
 }
